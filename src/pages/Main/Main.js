2f5d3dbf0948db2519e62a302c77af4ec606c0ff
suminllll <79704928+suminllll@users.noreply.github.com>
2021-10-08 11:11:42 +0900

    Add: 메인 레이아웃 구현중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import Nav from '../../components/Nav/Nav';
 import Img from './Img';
 import Scroll from './Scroll';
