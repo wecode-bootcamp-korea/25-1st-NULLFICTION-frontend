2a01b1f6cac148a4d36c2a8c436d84e1e0214d5c
suminllll <79704928+suminllll@users.noreply.github.com>
2021-10-08 19:57:09 +0900

    modify: 메인 레이아웃 구현중

--- src/pages/Main/Scroll.js
+++ src/pages/Main/Scroll.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, useRef } from 'react';
 import './Scroll.scss';
 import { Link } from 'react-router-dom';
 
 class Scroll extends Component {
+  // function RogoEvent () {
+  //   const scroll = useRef(null);
+
+  // function handleScroll() {
+  //   window.addEventListener('scroll', () => {
+  //     const value = window.scrollY;
+  //     scroll.current.style.middle = value + 'px';
+  //   })
+
+  //   }
+  // }
+
   render() {
     return (
       <section className="scrollSection">
         <div className="rogoScrollBox">
           <Link to="/">
-            <img
-              alt="rogoScroll"
-              src="/images/LogoScroll.png"
-              className="rogoScroll"
-            />
+            <img alt="rogoScroll" src="/images/LogoScroll.png" />
           </Link>
         </div>
       </section>
