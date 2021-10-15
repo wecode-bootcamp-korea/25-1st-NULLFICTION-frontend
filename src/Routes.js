11e5908e1b134e9f0c783d64febc10d964f577ab
jcy <poiuytasr@gmail.com>
2021-10-15 11:20:48 +0900

    [25기 정찬영] import 중복 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
-import Nav from '../src/components/Nav/Nav';
-import Footer from '../src/components/Footer/Footer';
 import Login from './pages/Login/Login';
 import Join from './pages/Join/Join';
 import Product from './pages/Product/Product';
